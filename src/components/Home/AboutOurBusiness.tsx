f74619a4c28736130a3332274fa54e6c716ae860
Erick Escagion <erickeescagion@gmail.com>
2021-08-24 12:33:48 -0300

    fix mobile/desktop responsive

--- src/components/Home/AboutOurBusiness.tsx
+++ src/components/Home/AboutOurBusiness.tsx
@@ -46,10 +46,10 @@ const useStyles = makeStyles(() =>
       fontWeight: 'bold',
       fontFamily: 'montserrat',
     },
-    background:{
+    background: {
       background: '#F2F7FA',
     },
-    img:{
+    img: {
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center'
@@ -59,46 +59,102 @@ const useStyles = makeStyles(() =>
 
 const AboutOurBusiness: React.FC = () => {
   const classes = useStyles();
+
+  function mobileOrDesktop(width: number) {
+    if (width > 1030) {
+      return (
+        <div className={classes.background}>
+          <br />
+          <br />
+          <div className={classes.container}>
+            <div className={classes.root}>
+              <Grid container spacing={10}>
+                <Grid item xs>
+                  <div className={classes.root2}>
+                    <Typography className={classes.title} paragraph={true}> Sobre o nosso negócio </Typography>
+                    <Typography className={classes.titlev2} paragraph={true}> Comprometimento com a excelência </Typography>
+                    <Typography className={classes.text} paragraph={true}> A <text className={classes.bold}>Likkn</text> é uma startup de tecnologia voltada à disponibilização de soluções para micro, pequenas e médias empresas. </Typography>
+                    <Typography className={classes.text} paragraph={true}> Temos em nosso DNA décadas de experiência implementando sistemas para grandes industrias. </Typography>
+                    <Typography className={classes.text} paragraph={true}> Aplicamos o que há de mais moderno no atendimento às necessidades dos nossos clientes. </Typography>
+                  </div >
+                </Grid>
+                <Grid item xs className={classes.img}>
+                  <div className={classes.root2}>
+                    <img src={experience} width="100%" height="100%" alt="experiencia" />
+                  </div >
+                </Grid>
+              </Grid>
+
+              <Grid container spacing={10}>
+                <Grid item xs className={classes.img}>
+                  <div className={classes.root2}>
+                    <img src={sobre} width="100%" height="100%" alt="sobre" />
+                  </div >
+                </Grid>
+                <Grid item xs>
+                  <div className={classes.root2}>
+                    <Typography className={classes.text} paragraph={true}> Os produtos e soluções likkn foram selecionados e desenvolvidos com o propósito de facilitar a informatização de negócios, além de otimizar processos e propor soluções em empresas que já tenham iniciado seu processo de informatização.</Typography>
+                    <Typography className={classes.text} paragraph={true}> Acreditamos que a tecnologia é o motor da economia. Computadores tomaram conta do nosso dia-a-dia, não fazemos nada sem interagirmos com algum programa de computador. </Typography>
+                    <Typography className={classes.text} > Também acreditamos que o acesso à tecnologia não é democrático para empresas: as melhores soluções são caras, seja a implementação e/ou manutenção, tornando impraticável para pequenos e médios empreendedores colherem todos os benefícios que a tecnologia tem a oferecer. </Typography>
+                  </div >
+                </Grid>
+              </Grid>
+            </div>
+          </div>
+          <br />
+        </div>
+      );
+    } else {
+      return (
+        <div className={classes.background}>
+          <br />
+          <br />
+          <div className={classes.container}>
+            <div className={classes.root}>
+              <Grid container spacing={10}>
+                <Grid item xs>
+                  <div className={classes.root2}>
+                    <Typography className={classes.title} paragraph={true}> Sobre o nosso negócio </Typography>
+                    <Typography className={classes.titlev2} paragraph={true}> Comprometimento com a excelência </Typography>
+                    <Typography className={classes.text} paragraph={true}> A <text className={classes.bold}>Likkn</text> é uma startup de tecnologia voltada à disponibilização de soluções para micro, pequenas e médias empresas. </Typography>
+                    <Typography className={classes.text} paragraph={true}> Temos em nosso DNA décadas de experiência implementando sistemas para grandes industrias. </Typography>
+                    <Typography className={classes.text} > Aplicamos o que há de mais moderno no atendimento às necessidades dos nossos clientes. </Typography>
+                  </div >
+                </Grid>
+                <Grid item xs className={classes.img}>
+                  <div className={classes.root2}>
+                    <img src={experience} width="100%" height="100%" alt="experiencia" />
+                  </div >
+                </Grid>
+              </Grid>
+              <br /><br /><br />
+              <Grid container spacing={10}>
+                <Grid item xs>
+                  <div className={classes.root2}>
+                    <Typography className={classes.text} paragraph={true}> Os produtos e soluções likkn foram selecionados e desenvolvidos com o propósito de facilitar a informatização de negócios, além de otimizar processos e propor soluções em empresas que já tenham iniciado seu processo de informatização.</Typography>
+                    <Typography className={classes.text} paragraph={true}> Acreditamos que a tecnologia é o motor da economia. Computadores tomaram conta do nosso dia-a-dia, não fazemos nada sem interagirmos com algum programa de computador. </Typography>
+                    <Typography className={classes.text} > Também acreditamos que o acesso à tecnologia não é democrático para empresas: as melhores soluções são caras, seja a implementação e/ou manutenção, tornando impraticável para pequenos e médios empreendedores colherem todos os benefícios que a tecnologia tem a oferecer. </Typography>
+                  </div >
+                </Grid>
+
+                <Grid item xs className={classes.img}>
+                  <div className={classes.root2}>
+                    <img src={sobre} width="100%" height="100%" alt="sobre" />
+                  </div >
+                </Grid>
+              </Grid>
+              <br /><br /><br />
+            </div>
+          </div>
+          <br />
+        </div>
+      );
+    }
+  }
+
   return (
     <div className={classes.background}>
-      <br/>
-      <br/>
-      <div className={classes.container}>
-        <div className={classes.root}>
-          <Grid container spacing={10}>
-            <Grid item xs>
-              <div className={classes.root2}>
-                <Typography className={classes.title} paragraph={true}> Sobre o nosso negócio </Typography>
-                <Typography className={classes.titlev2} paragraph={true}> Comprometimento com a excelência </Typography>
-                <Typography className={classes.text} paragraph={true}> A <text className={classes.bold}>Likkn</text> é uma startup de tecnologia voltada à disponibilização de soluções para micro, pequenas e médias empresas. </Typography>
-                <Typography className={classes.text} paragraph={true}> Temos em nosso DNA décadas de experiência implementando sistemas para grandes industrias. </Typography>
-                <Typography className={classes.text} paragraph={true}> Aplicamos o que há de mais moderno no atendimento às necessidades dos nossos clientes. </Typography>
-              </div >
-            </Grid>
-            <Grid item xs className={classes.img}>
-              <div className={classes.root2}>
-                  <img src={experience} width="100%" height="100%" alt="experiencia" />
-              </div >
-            </Grid>
-          </Grid>
-
-          <Grid container spacing={10}>
-            <Grid item xs className={classes.img}>
-              <div className={classes.root2}>
-                <img src={sobre} width="100%" height="100%" alt="sobre" />
-              </div >
-            </Grid>
-            <Grid item xs>
-              <div className={classes.root2}>
-                <Typography className={classes.text} paragraph={true}> Os produtos e soluções likkn foram selecionados e desenvolvidos com o propósito de facilitar a informatização de negócios, além de otimizar processos e propor soluções em empresas que já tenham iniciado seu processo de informatização.</Typography>
-                <Typography className={classes.text} paragraph={true}> Acreditamos que a tecnologia é o motor da economia. Computadores tomaram conta do nosso dia-a-dia, não fazemos nada sem interagirmos com algum programa de computador. </Typography>
-                <Typography className={classes.text} > Também acreditamos que o acesso à tecnologia não é democrático para empresas: as melhores soluções são caras, seja a implementação e/ou manutenção, tornando impraticável para pequenos e médios empreendedores colherem todos os benefícios que a tecnologia tem a oferecer. </Typography>
-              </div >
-            </Grid>
-          </Grid>
-        </div>
-      </div>
-      <br/>
+      {mobileOrDesktop(window.innerWidth)}
     </div>
   );
 }
